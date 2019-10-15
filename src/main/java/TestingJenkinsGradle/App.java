00b7f2a0ad27867cd473c3864b0ce367074488ef
root <root@instance-1.us-central1-a.c.kubernetes-237817.internal>
2019-10-15 20:11:12 +0000

    Add localtime

--- src/main/java/TestingJenkinsGradle/App.java
+++ src/main/java/TestingJenkinsGradle/App.java
@@ -3,6 +3,8 @@
  */
 package TestingJenkinsGradle;
 
+import org.joda.time.LocalTime;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
@@ -10,5 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+	LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
     }
 }
